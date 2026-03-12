146c27d4b182e751bf79ee25d25b7f222618da0b
Stephanie Sacker <55244155+sacker-s@users.noreply.github.com>
2026-03-12 16:01:03 +0000

    Update vite.config.js
    Update with repo name

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // Change the base path below to your GitHub repository name before deploying.
-// Example: if your repo is https://github.com/yourname/tiktok-customer-feed
-// then use base: "/tiktok-customer-feed/"
 export default defineConfig({
   plugins: [react()],
-  base: "/tiktok-customer-feed/",
+  base: "/tiktok/",
 });
